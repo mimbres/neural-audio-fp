a2fbc1ca1fb2cae1962839d9e092b9cdcaac3e71
mimbres <1ronyar@gmail.com>
2021-07-15 00:44:50 +0900

    fix #13: helper allows training by restoring checkpoints on other devices

--- model/utils/experiment_helper.py
+++ model/utils/experiment_helper.py
@@ -12,18 +12,18 @@ class ExperimentHelper():
     Experiment Helper class for conducting experiment.
     An object of this class manages:
         - initializing/restoring model checkpoints with optimizer states,
-        - logging and writing loss metrics and images to Tensorboard. 
-    
+        - logging and writing loss metrics and images to Tensorboard.
+
     USAGE:
-        1. Define a new or existing (for restoring) experiment name. 
+        1. Define a new or existing (for restoring) experiment name.
         2. Construct {optimizer, model}.
         3. Get the number of steps for a single epoech from dataloader.
         4. Construct a helper class object.
         5. In your training loop, call the methods described in the below.
-    
+
     Methods:
         - save_checkpoint():
-            Save current model and optimizer states to checkpoint. 
+            Save current model and optimizer states to checkpoint.
         - update_on_epoch_end():
             Update current epoch index, and loss metrics.
         - update_tr_loss(value, tb=True)->average_loss:
@@ -33,7 +33,7 @@ class ExperimentHelper():
         - update_minitest_acc(accs_by_scope, scopes, key, tb=True):
         - write_image_tensorboard(key, image_mtx):
             key: (str) ex) 'tr_sim_mtx'
-            image: (2d numpy array)     
+            image: (2d numpy array)
     """
     def __init__(
             self,
@@ -64,7 +64,7 @@ class ExperimentHelper():
         self._checkpoint_name = checkpoint_name
         self._cfg_use_tensorboard = cfg['TRAIN']['TENSORBOARD']
         self.epoch = 1 # Initialize current epoch.
-        
+
         # Directories
         if cfg['DIR']['LOG_ROOT_DIR']:
             _root_dir = cfg['DIR']['LOG_ROOT_DIR']
@@ -77,26 +77,26 @@ class ExperimentHelper():
         self._tr_loss = K.metrics.Mean(name='train_loss')
         self._val_loss = K.metrics.Mean(name='val_loss')
         self._minitest_acc = None
-        
+
         # Tensorboard writers
         self._tr_summary_writer = create_file_writer(self._log_dir + '/train')
         self._val_summary_writer = create_file_writer(self._log_dir + '/val')
         self._minitest_summary_writer_dict = dict()
-        for key in ['f', 'L2(f)', 'g(f)']: 
+        for key in ['f', 'L2(f)', 'g(f)']:
             self._minitest_summary_writer_dict[key] = create_file_writer(
-                self._log_dir + '/mini_test/' + key) 
+                self._log_dir + '/mini_test/' + key)
         self._image_writer = create_file_writer(self._log_dir + '/images')
-            
+
         # Assign optimizer and model to checkpoint
         self.optimizer = optimizer # assign, not to create.
         self._model_to_checkpoint = model_to_checkpoint # assign, not to create.
-            
+
         # Setup checkpoint manager
         if cfg:
             self._checkpoint_keep_n_hour = int(cfg['TRAIN']['CHECKPOINT_KEEP_N_HOUR'])
         else:
             self._checkpoint_keep_n_hour = 1 # every 1 hours
-        
+
         self._checkpoint = tf.train.Checkpoint(
             optimizer=optimizer,
             model=model_to_checkpoint
@@ -107,17 +107,10 @@ class ExperimentHelper():
             max_to_keep=3,
             keep_checkpoint_every_n_hours=self._checkpoint_keep_n_hour,
             step_counter=self.optimizer.iterations)
-            
-        # Restore or initialize checkpoint
-        if self.c_manager.latest_checkpoint:
-            tf.print("---Restoring from {}---".format(
-                self.c_manager.latest_checkpoint))
-            self._checkpoint.restore(self.c_manager.latest_checkpoint)
-            self.epoch = int(self.c_manager.latest_checkpoint.split(sep='ckpt-')[-1])
-        else:
-            tf.print("-----------Initializing from scratch-----------")
-        
-            
+
+        self.load_checkpoint()
+
+
     def update_on_epoch_end(self, save_checkpoint_now=True):
         """ Update current epoch index, and loss metrics. """
         if save_checkpoint_now:
@@ -129,11 +122,25 @@ class ExperimentHelper():
         self.epoch += 1
 
 
+    def load_checkpoint(self):
+        """ Try loading a saved checkpoint. If no checkpoint, initialize from
+            scratch.
+        """
+        if self.c_manager.latest_checkpoint:
+            tf.print("---Restoring from {}---".format(
+                self.c_manager.latest_checkpoint))
+            status = self._checkpoint.restore(self.c_manager.latest_checkpoint)
+            status.expect_partial()
+            self.epoch = int(self.c_manager.latest_checkpoint.split(sep='ckpt-')[-1])
+        else:
+            tf.print("-----------Initializing from scratch-----------")
+
+
     def save_checkpoint(self):
         """Save current model and optimizer states to checkpoint."""
-        self.c_manager.save()        
-                
-    
+        self.c_manager.save()
+
+
     def update_tr_loss(self, value, tb=None):
         """
         Parameters
@@ -155,8 +162,8 @@ class ExperimentHelper():
         else:
             pass
         return avg_tr_loss
-        
-        
+
+
     def update_val_loss(self, value, tb=None):
         """
         Parameters
@@ -178,8 +185,8 @@ class ExperimentHelper():
         else:
             pass
         return avg_val_loss
-        
-    
+
+
     def update_minitest_acc(self, accs_by_scope, scopes, key, tb=None):
         """
         Parameters
@@ -193,7 +200,7 @@ class ExperimentHelper():
             'g(f)' or 'f' or 'f_postL2'.
         tb : (bool), optional
             Write to tensorboard if set True. The default is True.
-            
+
         """
         self._minitest_acc = accs_by_scope
         if tb or (tb==None and self._cfg_use_tensorboard):
@@ -205,16 +212,16 @@ class ExperimentHelper():
 
     def write_image_tensorboard(self, key, image_mtx):
         """
-        Generate and write an image to tensorboard.  
+        Generate and write an image to tensorboard.
 
         Parameters
         ----------
-        key: (str) 
+        key: (str)
             A key string to specify an image source. ex) 'tr_sim_mtx'
         image_mtx : (2D NumPy array)
             Any 2D matrix representing an image.
-            
-        """        
+
+        """
         if self._cfg_use_tensorboard:
             # Generate images
             img = get_imshow_image(image_mtx, f'{key}, Epoch={self.epoch}')
